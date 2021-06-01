c2fac6bd4e593b8c9809a997e367421ac289eb9d
James <jaames.chen@gmail.com>
2021-06-01 11:20:59 -0400

    created default Board

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+    render() {
+        return (
+            <div class="col border border-2 border-dark">
+                <h1>Word</h1>
+            </div>
+        )
+    }
+}
+
+export default Card
