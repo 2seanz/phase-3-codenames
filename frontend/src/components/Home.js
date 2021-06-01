1c5889915436ba9775aa402a518b265d5bef3010
Sean Ngo <seanzngo@gmail.com>
2021-06-01 09:48:18 -0400

    form stuff

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,9 +10,25 @@ import FormControl from 'react-bootstrap/FormControl'
 
 class Home extends Component {
 
+    state = {
+        playerName: 0
+    }
+
     handleClick = (event,num) => {
         console.log(event.target)
         console.log(num)
+        this.setState({playerName: num})
+    }
+
+    displayForm = () => {
+        // console.log("we made it bishhh")
+
+         const ourForm = Array(this.state.playerName).fill().map((item, index) => (
+            <Form.Group controlId="playerNames">                
+                <Form.Control type="name" placeholder={`Enter Name of Player ${index + 1}`} />
+            </Form.Group>
+        ))
+        return ourForm
     }
 
     render() {
@@ -56,16 +72,8 @@ class Home extends Component {
                     {/* <Dropdown.Item eventKey="4">Separated link</Dropdown.Item> */}
                     </DropdownButton>
                     <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="name" placeholder="Enter name" />
-                            <Form.Text className="text-muted">
-                            We'll never share your name with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-                        {/* <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group> */}
+                        <Form.Label>Name</Form.Label>
+                        {this.displayForm()}
                         <Button variant="primary" type="submit">Submit</Button>
                     </Form>
                 </div>
